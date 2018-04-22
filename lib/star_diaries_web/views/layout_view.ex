45cbf10497cc2a0445813028d10ade83492eef43
Kacper Golinski <kacper.golinski@gmail.com>
2018-04-22 19:32:59 +0200

    create `show_flash_message`  helper function

--- lib/star_diaries_web/views/layout_view.ex
+++ lib/star_diaries_web/views/layout_view.ex
@@ -1,3 +1,23 @@
 defmodule StarDiariesWeb.LayoutView do
   use StarDiariesWeb, :view
+
+  def show_flash_message(conn, key) do
+    conn
+    |> get_flash(key)
+    |> render_flash(key)
+  end
+
+  defp render_flash(nil, _),      do: nil
+  defp render_flash(msg, :info),  do: render_flash_template(msg, :info)
+  defp render_flash(msg, :error), do: render_flash_template(msg, :danger)
+  defp render_flash(_, _),        do: nil
+
+  defp render_flash_template(message, flash_class) do
+    Phoenix.View.render(
+      StarDiariesWeb.LayoutView,
+      "_flash_message.html",
+      message: message,
+      flash_class: flash_class
+    )
+  end
 end
