7ccf8df3f340ad7472d6ae178a67dc4e1d6d43eb
Kacper Golinski <kacper.golinski@gmail.com>
2018-04-22 19:29:41 +0200

    remove boilerplate add sass and bootstrap@4

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,19 +1,20 @@
-// We need to import the CSS so that webpack will load it.
+import 'phoenix_html'
+
+// We need to import the SCSS so that webpack will load it.
 // The MiniCssExtractPlugin is used to separate it out into
 // its own CSS file.
-import css from "../css/app.css";
+import '../scss/app.scss'
 
 // webpack automatically bundles all modules in your
 // entry points. Those entry points can be configured
-// in "webpack.config.js".
+// in 'webpack.config.js'.
 //
 // Import dependencies
 //
-import "phoenix_html"
 
 // Import local files
 //
 // Local files can be imported directly using relative
-// paths "./socket" or full ones "web/static/js/socket".
+// paths './socket' or full ones 'web/static/js/socket'.
 
-// import socket from "./socket"
+// import socket from './socket'
--- assets/webpack.config.js
+++ assets/webpack.config.js
@@ -1,20 +1,20 @@
-const path = require('path');
-const MiniCssExtractPlugin = require('mini-css-extract-plugin');
-const UglifyJsPlugin = require('uglifyjs-webpack-plugin');
-const OptimizeCSSAssetsPlugin = require('optimize-css-assets-webpack-plugin');
-const CopyWebpackPlugin = require('copy-webpack-plugin');
+const path = require('path')
+const MiniCssExtractPlugin = require('mini-css-extract-plugin')
+const UglifyJsPlugin = require('uglifyjs-webpack-plugin')
+const OptimizeCSSAssetsPlugin = require('optimize-css-assets-webpack-plugin')
+const CopyWebpackPlugin = require('copy-webpack-plugin')
 
-module.exports = (env, options) => ({
+module.exports = (/* env, options */) => ({
   optimization: {
     minimizer: [
       new UglifyJsPlugin({ cache: true, parallel: true, sourceMap: false }),
-      new OptimizeCSSAssetsPlugin({})
-    ]
+      new OptimizeCSSAssetsPlugin({}),
+    ],
   },
   entry: './js/app.js',
   output: {
     filename: 'app.js',
-    path: path.resolve(__dirname, '../priv/static/js')
+    path: path.resolve(__dirname, '../priv/static/js'),
   },
   module: {
     rules: [
@@ -22,17 +22,21 @@ module.exports = (env, options) => ({
         test: /\.js$/,
         exclude: /node_modules/,
         use: {
-          loader: 'babel-loader'
-        }
+          loader: 'babel-loader',
+        },
       },
       {
         test: /\.css$/,
-        use: [MiniCssExtractPlugin.loader, 'css-loader']
-      }
-    ]
+        use: [MiniCssExtractPlugin.loader, 'css-loader'],
+      },
+      {
+        test: /\.scss$/,
+        use: [MiniCssExtractPlugin.loader, 'css-loader', 'sass-loader'],
+      },
+    ],
   },
   plugins: [
     new MiniCssExtractPlugin({ filename: '../css/app.css' }),
-    new CopyWebpackPlugin([{ from: 'static/', to: '../' }])
-  ]
-});
+    new CopyWebpackPlugin([{ from: 'static/', to: '../' }]),
+  ],
+})
