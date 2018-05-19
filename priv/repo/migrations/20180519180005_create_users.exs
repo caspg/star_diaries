cdeb808b0a217ce4ce4a9d1eedf939110e298c64
Kacper Golinski <kacper.golinski@gmail.com>
2018-05-19 20:02:37 +0200

    generate accounts context

---
+++ priv/repo/migrations/20180519180005_create_users.exs
@@ -0,0 +1,14 @@
+defmodule StarDiaries.Repo.Migrations.CreateUsers do
+  use Ecto.Migration
+
+  def change do
+    create table(:users) do
+      add :name, :string
+      add :email, :string
+
+      timestamps()
+    end
+
+    create unique_index(:users, [:email])
+  end
+end
